6d70b70adc771ab838d468782778b5fd6de7941b
Kazi Tania <kazitania.reg@gmail.com>
2021-01-22 17:45:59 +0600

    comment done.

--- assignment.js
+++ assignment.js
@@ -1,13 +1,25 @@
 
 function kilometerToMeter(kilometer){
+    if (kilometer<=0){
+        return "Distance can not be negetive."
+    }
     var meter = kilometer*1000;
     return meter;
 }
 //  This lines are not required for assignment
-//  var resultmeter = kilometerToMeter(14);
-//  console.log(resultmeter);
+//   var resultmeter = kilometerToMeter(-2);
+//   console.log(resultmeter);
 
 function budgetCalculator(watchValue,phoneValue,laptopValue){
+    if(watchValue<0){
+        return "Values can not be negetive.";
+    }
+    if(phoneValue<0){
+        return "Values can not be negetive.";
+    }
+    if(laptopValue<0){
+        return "Values can not be negetive.";
+    }
      var watchPrize = watchValue*50;
      var phonePrize = phoneValue*100;
      var laptopPrize = laptopValue*500;
@@ -16,38 +28,43 @@ function budgetCalculator(watchValue,phoneValue,laptopValue){
      return totalPrize;
 }
 // This lines are not required for assignment.
-// var totalBudget = budgetCalculator();
-// console.log(totalBudget);
+//  var totalBudget = budgetCalculator(10,10,10);
+//  console.log(totalBudget);
 
  function hotelCost(days){
-    var totalCost = 0;
+    
+     if(days<=0){
+        return "Days can not be negetive";
+     }
+     var totalCost = 0;
     if(days<=10){
         totalCost = days * 100;
     }
+    
     else if(days<=20){
-        var firstTenDays = days * 100;
+        var firstTenDays = 10 * 100;
         var remaining = days - 10;
         var secondTenDays = remaining * 80;
         totalCost = firstTenDays + secondTenDays;
     }
     else{
-        var firstTenDays = days * 100;
-        var secondTenDays = days * 80;
+        var firstTenDays = 10 * 100;
+        var secondTenDays = 10 * 80;
         var remaining = days-20;
-        var lastDays = days * 50;
+        var lastDays = remaining * 50;
         totalCost = firstTenDays + secondTenDays + lastDays;
     }
     return totalCost;
  }
 //  This lines are not required for assignment.
-//  var findCost = hotelCost(20);
+//  var findCost = hotelCost(41);
 //  console.log(findCost);
 
-
-
-
-
 function megaFriend(friendName){
+    if(friendName<=0){
+        return "Name can not be negetive."
+
+    }
     var longestName = friendName[0];
     
     for(var i = 0 ; i < friendName.length; i++){
@@ -59,6 +76,6 @@ function megaFriend(friendName){
     return longestName;
 }
 // This lines are not required for assignment.
-// var findname = megaFriend(["tania", "ilma" , "supti","Suchi","Niharika", "karima","Maksuda","nazmun", "sabikun"]);
-// console.log(findname);
+//  var findname = megaFriend(["tania", "ilma" , "supti","Suchi","Niharika", "karima","Maksuda","nazmun", "sabikun"]);
+//  console.log(findname);
 
